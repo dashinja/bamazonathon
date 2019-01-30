3aa6fd6adf037fdfd4163d6ede61d13a2a24d1ca
DaShinja <byron@dashinja.com>
2019-01-30 10:15:51 -0500

    setup a select all query - needs testing

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,6 +1,6 @@
-const mysql = require('mysql')
-const inquirer = require('inquirer')
-require('dotenv').config()
+const mysql = require('mysql');
+const inquirer = require('inquirer');
+require('dotenv').config();
 
 const connection = mysql.createConnection({
   host: 'localhost',
@@ -8,11 +8,24 @@ const connection = mysql.createConnection({
   user: process.env.SQLUSER,
   password: process.env.SQLPW,
   database: 'bamazon_db'
-})
+});
 
 connection.connect(err => {
-  if (err) throw err
-  console.log(`Connected with id: ${connection.threadId}`)
+  if (err) throw err;
+  console.log(`Connected with id: ${connection.threadId}`);
 
-  connection.end()
-})
+  startUp();
+  connection.end();
+});
+
+function startUp() {
+  console.log('Here are our products...');
+
+  let sqlQuery =
+    'SELECT (item_id, product_name, price, stock_quantity) FROM products;';
+  let query = connection.query(sqlQuery, (err, res) => {
+    if (err) throw err;
+    console.log(res);
+  });
+  // inquirer.prompt([{}]);
+}
